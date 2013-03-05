0ce933c188db9e1baa707ceedb1331ca153572a2
Aaron Clinger <aaron.clinger@venablesbell.com>
2013-03-04 18:41:01 -0800

    Started styling site.

--- application/views/main.php
+++ application/views/main.php
@@ -3,12 +3,22 @@
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 		<title>Sourcer</title>
+		<link rel="stylesheet" href="/assets/css/styles.css" type="text/css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+<?php
+	if (isset($source))
+	{
+		echo '<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>';
+	}
+?>
+		
 	</head>
 	<body>
-		<form action="/" method="post" accept-charset="utf-8" id="sign-in-form">
-			<fieldset>
-				<legend>Sourcer</legend>
-				
+		<header>
+			<h1>Sourcer</h1>
+			<p>A web utlity to fetch and view the source of a remote URL. Useful for debugging on mobile or across user agents.</p>
+			
+			<form action="/" method="post" accept-charset="utf-8" id="sign-in-form">
 				<dl>
 					<dt><label for="url">URL to Fetch:</label></dt>
 						<dd><input type="url" name="url" id="url" value="<?=set_value('url', 'http://');?>"></dd>
@@ -29,14 +39,16 @@
 				</dl>
 				
 				<button type="submit">Fetch Source</button>
-			</fieldset>
-		</form>
+			</form>
+		</header>
 <?php
 	if (isset($source))
 	{
-		echo '<pre><code>' . htmlentities($source) . '</code></pre>';
+		echo '<pre><code class="prettyprint linenums">' . htmlentities($source) . '</code></pre>';
 	}
 ?>
-
+		<footer>
+			<p>Built by <a href="http://aaronclinger.com/" target="_blank">Aaron</a> &amp; <a href="http://lucasishuman.com/" target="_blank">Lucas</a>. Powered by <a href="http://ellislab.com/codeigniter" target="_blank">CodeIgniter</a> &amp; <a href="http://code.google.com/p/google-code-prettify/" target="_blank">Google Code Prettify</a>. Source on <a href="https://github.com/aaronclinger/sourcer" target="_blank">GitHub</a>.</p>
+		</footer>
 	</body>
 </html>
